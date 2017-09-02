17cd461a5179fde81e990549d57d2bbe7ff1aa6c
dasturchiuz <dasturchiuz@gmail.com>
2017-09-02 18:04:57 +0500

    Yangiladim soat 02092017 18:04

--- js/jquery.maphilight.js
+++ js/jquery.maphilight.js
@@ -17,7 +17,7 @@
 		$.fn.maphilight = function() { return this; };
 		return;
 	}
-	
+
 	if(has_canvas) {
 		hex_to_decimal = function(hex) {
 			return Math.max(0, Math.min(parseInt(hex, 16), 255));
@@ -26,14 +26,14 @@
 			return 'rgba('+hex_to_decimal(color.substr(0,2))+','+hex_to_decimal(color.substr(2,2))+','+hex_to_decimal(color.substr(4,2))+','+opacity+')';
 		};
 		create_canvas_for = function(img) {
-			var c = $('<canvas style="width:'+img.width+'px;height:'+img.height+'px;"></canvas>').get(0);
-			c.getContext("2d").clearRect(0, 0, c.width, c.height);
+			var c = $('<canvas style="width:'+$(img).width()+'px;height:'+$(img).height()+'px;"></canvas>').get(0);
+			c.getContext("2d").clearRect(0, 0, $(img).width(), $(img).height());
 			return c;
 		};
 		var draw_shape = function(context, shape, coords, x_shift, y_shift) {
 			x_shift = x_shift || 0;
 			y_shift = y_shift || 0;
-			
+
 			context.beginPath();
 			if(shape == 'rect') {
 				// x, y, width, height
@@ -48,12 +48,12 @@
 				context.arc(coords[0] + x_shift, coords[1] + y_shift, coords[2], 0, Math.PI * 2, false);
 			}
 			context.closePath();
-		}
+		};
 		add_shape_to = function(canvas, shape, coords, options, name) {
 			var i, context = canvas.getContext('2d');
-			
+
 			// Because I don't want to worry about setting things back to a base state
-			
+
 			// Shadow has to happen first, since it's on the bottom, and it does some clip /
 			// fill operations which would interfere with what comes next.
 			if(options.shadow) {
@@ -63,19 +63,19 @@
 					draw_shape(context, shape, coords);
 					context.clip();
 				}
-				
+
 				// Redraw the shape shifted off the canvas massively so we can cast a shadow
 				// onto the canvas without having to worry about the stroke or fill (which
 				// cannot have 0 opacity or width, since they're what cast the shadow).
 				var x_shift = canvas.width * 100;
 				var y_shift = canvas.height * 100;
 				draw_shape(context, shape, coords, x_shift, y_shift);
-				
+
 				context.shadowOffsetX = options.shadowX - x_shift;
 				context.shadowOffsetY = options.shadowY - y_shift;
 				context.shadowBlur = options.shadowRadius;
 				context.shadowColor = css3color(options.shadowColor, options.shadowOpacity);
-				
+
 				// Now, work out where to cast the shadow from! It looks better if it's cast
 				// from a fill when it's an outside shadow or a stroke when it's an interior
 				// shadow. Allow the user to override this if they need to.
@@ -95,7 +95,7 @@
 					context.fill();
 				}
 				context.restore();
-				
+
 				// and now we clean up
 				if(options.shadowPosition == "outside") {
 					context.save();
@@ -107,11 +107,11 @@
 					context.restore();
 				}
 			}
-			
+
 			context.save();
-			
+
 			draw_shape(context, shape, coords);
-			
+
 			// fill has to come after shadow, otherwise the shadow will be drawn over the fill,
 			// which mostly looks weird when the shadow has a high opacity
 			if(options.fill) {
@@ -125,9 +125,9 @@
 				context.lineWidth = options.strokeWidth;
 				context.stroke();
 			}
-			
+
 			context.restore();
-			
+
 			if(options.fade) {
 				$(canvas).css('opacity', 0).animate({opacity: 1}, 100);
 			}
@@ -156,13 +156,13 @@
 			$(canvas).append(e);
 		};
 		clear_canvas = function(canvas) {
-			// jquery1.8 + ie7 
+			// jquery1.8 + ie7
 			var $html = $("<div>" + canvas.innerHTML + "</div>");
 			$html.children('[name=highlighted]').remove();
 			canvas.innerHTML = $html.html();
 		};
 	}
-	
+
 	shape_from_area = function(area) {
 		var i, coords = area.getAttribute('coords').split(',');
 		for (i=0; i < coords.length; i++) { coords[i] = parseFloat(coords[i]); }
@@ -173,7 +173,7 @@
 		var $area = $(area);
 		return $.extend({}, options, $.metadata ? $area.metadata() : false, $area.data('maphilight'));
 	};
-	
+
 	is_image_loaded = function(img) {
 		if(!img.complete) { return false; } // IE
 		if(typeof img.naturalWidth != "undefined" && img.naturalWidth === 0) { return false; } // Others
@@ -187,11 +187,11 @@
 		padding: 0,
 		border: 0
 	};
-	
+
 	var ie_hax_done = false;
 	$.fn.maphilight = function(opts) {
 		opts = $.extend({}, $.fn.maphilight.defaults, opts);
-		
+
 		if(!has_canvas && !ie_hax_done) {
 			$(window).ready(function() {
 				document.namespaces.add("v", "urn:schemas-microsoft-com:vml");
@@ -205,9 +205,9 @@
 			});
 			ie_hax_done = true;
 		}
-		
+
 		return this.each(function() {
-			var img, wrap, options, map, canvas, canvas_always, mouseover, highlighted_shape, usemap;
+			var img, wrap, options, map, canvas, canvas_always, highlighted_shape, usemap;
 			img = $(this);
 
 			if(!is_image_loaded(this)) {
@@ -224,7 +224,7 @@
 			usemap = img.get(0).getAttribute('usemap');
 
 			if (!usemap) {
-				return
+				return;
 			}
 
 			map = $('map[name="'+usemap.substr(1)+'"]');
@@ -239,17 +239,18 @@
 				var wrapper = img.parent();
 				img.insertBefore(wrapper);
 				wrapper.remove();
-				$(map).unbind('.maphilight').find('area[coords]').unbind('.maphilight');
+				$(map).unbind('.maphilight');
 			}
 
 			wrap = $('<div></div>').css({
 				display:'block',
-				background:'url("'+this.src+'")',
+				backgroundImage:'url("'+this.src+'")',
+				backgroundSize:'contain',
 				position:'relative',
 				padding:0,
 				width:this.width,
 				height:this.height
-				});
+			});
 			if(options.wrapClass) {
 				if(options.wrapClass === true) {
 					wrap.addClass($(this).attr('class'));
@@ -260,47 +261,11 @@
 			img.before(wrap).css('opacity', 0).css(canvas_style).remove();
 			if(has_VML) { img.css('filter', 'Alpha(opacity=0)'); }
 			wrap.append(img);
-			
+
 			canvas = create_canvas_for(this);
 			$(canvas).css(canvas_style);
 			canvas.height = this.height;
 			canvas.width = this.width;
-			
-			mouseover = function(e) {
-				var shape, area_options;
-				area_options = options_from_area(this, options);
-				if(
-					!area_options.neverOn
-					&&
-					!area_options.alwaysOn
-				) {
-					shape = shape_from_area(this);
-					add_shape_to(canvas, shape[0], shape[1], area_options, "highlighted");
-					if(area_options.groupBy) {
-						var areas;
-						// two ways groupBy might work; attribute and selector
-						if(/^[a-zA-Z][\-a-zA-Z]+$/.test(area_options.groupBy)) {
-							areas = map.find('area['+area_options.groupBy+'="'+$(this).attr(area_options.groupBy)+'"]');
-						} else {
-							areas = map.find(area_options.groupBy);
-						}
-						var first = this;
-						areas.each(function() {
-							if(this != first) {
-								var subarea_options = options_from_area(this, options);
-								if(!subarea_options.neverOn && !subarea_options.alwaysOn) {
-									var shape = shape_from_area(this);
-									add_shape_to(canvas, shape[0], shape[1], subarea_options, "highlighted");
-								}
-							}
-						});
-					}
-					// workaround for IE7, IE8 not rendering the final rectangle in a group
-					if(!has_canvas) {
-						$(canvas).append('<v:rect></v:rect>');
-					}
-				}
-			}
 
 			$(map).bind('alwaysOn.maphilight', function() {
 				// Check for areas with alwaysOn set. These are added to a *second* canvas,
@@ -331,23 +296,50 @@
 						}
 					}
 				});
-			});
-			
-			$(map).trigger('alwaysOn.maphilight').find('area[coords]')
-				.bind('mouseover.maphilight', mouseover)
-				.bind('mouseout.maphilight', function(e) { clear_canvas(canvas); });
-			
+			}).trigger('alwaysOn.maphilight')
+				.bind('mouseover.maphilight, focus.maphilight', function(e) {
+					var shape, area_options, area = e.target;
+					area_options = options_from_area(area, options);
+					if(!area_options.neverOn && !area_options.alwaysOn) {
+						shape = shape_from_area(area);
+						add_shape_to(canvas, shape[0], shape[1], area_options, "highlighted");
+						if(area_options.groupBy) {
+							var areas;
+							// two ways groupBy might work; attribute and selector
+							if(/^[a-zA-Z][\-a-zA-Z]+$/.test(area_options.groupBy)) {
+								areas = map.find('area['+area_options.groupBy+'="'+$(area).attr(area_options.groupBy)+'"]');
+							} else {
+								areas = map.find(area_options.groupBy);
+							}
+							var first = area;
+							areas.each(function() {
+								if(this != first) {
+									var subarea_options = options_from_area(this, options);
+									if(!subarea_options.neverOn && !subarea_options.alwaysOn) {
+										var shape = shape_from_area(this);
+										add_shape_to(canvas, shape[0], shape[1], subarea_options, "highlighted");
+									}
+								}
+							});
+						}
+						// workaround for IE7, IE8 not rendering the final rectangle in a group
+						if(!has_canvas) {
+							$(canvas).append('<v:rect></v:rect>');
+						}
+					}
+				}).bind('mouseout.maphilight, blur.maphilight', function(e) { clear_canvas(canvas); });
+
 			img.before(canvas); // if we put this after, the mouseover events wouldn't fire.
-			
+
 			img.addClass('maphilighted');
 		});
 	};
 	$.fn.maphilight.defaults = {
 		fill: true,
 		fillColor: '000000',
-		fillOpacity: 0.4,
+		fillOpacity: 0.2,
 		stroke: true,
-		strokeColor: 'ffffff',
+		strokeColor: 'ff0000',
 		strokeOpacity: 1,
 		strokeWidth: 1,
 		fade: true,
@@ -366,73 +358,3 @@
 		shadowFrom: false
 	};
 })(jQuery);
-
-
-$(function() {
-        $('.map').maphilight();
-        $('#newlink_link').mouseover(function(e) {
-            $('#newlink').mouseover();
-        }).mouseout(function(e) {
-            $('#newlink').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-        $('#siritem5774').mouseover(function(e) {
-            $('#boyovut').mouseover();
-        }).mouseout(function(e) {
-            $('#boyovut').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#shovot_link').mouseover(function(e) {
-            $('#shovot').mouseover();
-        }).mouseout(function(e) {
-            $('#shovot').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#urganch_t_link').mouseover(function(e) {
-            $('#urganch').mouseover();
-        }).mouseout(function(e) {
-            $('#urganch').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-
-    $('#urganch_sh_link').mouseover(function(e) {
-            $('#urganch').mouseover();
-        }).mouseout(function(e) {
-            $('#urganch').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#xonqa_link').mouseover(function(e) {
-            $('#xonqa').mouseover();
-        }).mouseout(function(e) {
-            $('#xonqa').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#qushkupir_link').mouseover(function(e) {
-            $('#qushkupir').mouseover();
-        }).mouseout(function(e) {
-            $('#qushkupir').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#xiva_link').mouseover(function(e) {
-            $('#xiva').mouseover();
-        }).mouseout(function(e) {
-            $('#xiva').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#yangiariq_link').mouseover(function(e) {
-            $('#yangiariq').mouseover();
-        }).mouseout(function(e) {
-            $('#yangiariq').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#bogot_link').mouseover(function(e) {
-            $('#bogot').mouseover();
-        }).mouseout(function(e) {
-            $('#bogot').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    
-    $('#xozorasp_link').mouseover(function(e) {
-            $('#xozorasp').mouseover();
-        }).mouseout(function(e) {
-            $('#xozorasp').mouseout();
-        }).click(function(e) { e.preventDefault(); });
-    });
